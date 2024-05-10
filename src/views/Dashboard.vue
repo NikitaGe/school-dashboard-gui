63b01b606ec2dad0f532b6c928eeeb04ea241a9f
NikitaGe <nikitage2002@gmail.com>
2024-05-10 11:29:16 +0200

    push

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,21 +2,23 @@
     <v-row class="ma-2">
 
 
-      <v-col cols="12" sm="6" md="4">
-          <v-card>
+      <v-col cols="12" sm="6" md="6" lg="4">
+          <v-card class="elevation-5">
             <kalendar></kalendar>
           </v-card>
       </v-col>
 
-<v-col cols="12" sm="6" md="4">
+<v-col cols="12" sm="6" md="6" lg="4">
    
       <v-card height="525" 
-      title="News" 
-      subtitle="Parkplatz Sperrungen"
-      text="dwwddwdddwd"
+      class="elevation-5"
+      title="Aktuelles" 
+      :subtitle="news.ueberschrift"
+      :text="news.newstext"
       >
+      <v-divider></v-divider>
         <div style="height: 5rem;" class="ma-5">
-            <img src="https://tgbbz1.de/images/bilder/schule/turm.png" height="300" width="100%">
+            <img src="https://tgbbz1.de/images/bilder/schule/turm.png" height="230" width="100%">
         </div>
 
     </v-card>  
@@ -26,9 +28,10 @@
    
 </v-col>
 
-<v-col cols="12" md="4">
-   <v-card height="525" >
+<v-col cols="12" md="6" lg="4">
+   <v-card height="525" class="elevation-5">
     <h3 class="ma-3">Lehrer-Übersicht</h3>
+    <v-divider></v-divider>
      <lehrer-tabelle></lehrer-tabelle>
  </v-card>  
 </v-col>
@@ -48,6 +51,7 @@
   import SchuelerTabelle from '@/components/Widgets/SchuelerTabelle.vue';
   import LehrerTabelle from '@/components/Widgets/LehrerTabelle.vue';
 import store from '@/store';
+import { mapState } from 'vuex';
 
   export default defineComponent({
     name: 'dashboard-view',
@@ -64,7 +68,14 @@ import store from '@/store';
     async mounted() {
       await store.dispatch("getNews");
      
+    },
+
+
+    computed: {
+      ...mapState(["news"])
+
     }
+
 
 
 
